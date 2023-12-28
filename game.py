b25787bf8b0ee1bea7d31110af77b9c9bd8e0893
Christian Carbeau <ctc53@duke.edu>
2023-12-28 17:03:06 -0500

    game-probably-done

--- game.py
+++ game.py
@@ -2,11 +2,18 @@ import Deck
 import handValue
 
 def main():
-    deck = Deck.Deck()
-    round(deck.deck)
+    stack = int(input("What's your buy in? \n"))
+    while stack > 0: 
+        stack = round(stack)
+        print("Your stack is:", stack)
     
 def round(stack):
-    bet = input("How much would you like to bet? \n")
+    bet = int(input("How much would you like to bet? \n"))
+    while bet > stack and bet > 0: 
+        print("Please bet a valid amount")
+        bet = int(input("How much would you like to bet? \n"))
+        if bet == -1:
+            return -1
     deck = Deck.Deck()
     deck.deal(deck.deck)
     handVal = handValue.handValue.handVal(deck.player)
@@ -14,7 +21,7 @@ def round(stack):
     print("You:", deck.player[0]+" "+deck.player[1])
     if handVal[0] == 21: 
         print("Blackjack! You win: ",float(bet)*1.5)
-        return 
+        return stack + float(bet)*1.5
     move = input("Hit or Stay? \n")
     while (move == "hit" or move == "Hit") and handVal[1] < 22:
         if move == "hit" or move == "Hit":
@@ -22,56 +29,70 @@ def round(stack):
         print("Your Hand:", deck.player)
         if handValue.handValue.handVal(deck.player)[1] > 21:
             print('You lose',bet)
-            return
+            return stack - bet 
         move = input("Hit or Stay? \n")
         handVal = handValue.handValue.handVal(deck.player)
     print("Dealer's hand:",deck.dealer)
-    while handValue.handValue.handVal(deck.dealer)[1] < 17: 
+    while (handValue.handValue.handVal(deck.dealer)[0] < 17) or (handValue.handValue.handVal(deck.dealer)[0] > 21 and handValue.handValue.handVal(deck.dealer)[1] < 18): 
         deck.dealHit(deck.dealer,deck.deck)
         print(deck.dealer)
-    print(detWin(deck.dealer,handVal, bet))
+    result = detWin(deck.dealer,handVal, bet)
+    return stack + result 
+    
 
 def detWin(dealer, handVal, bet):
     if handValue.handValue.handVal(dealer)[1] > 21:
-        return "You win: " + bet
+        return bet
     
     # Using player's soft count
     elif handVal[0] > 21: 
         # Comparing player's soft count to dealer's soft count
         if handValue.handValue.handVal(dealer)[0] > 21:
             if handVal[1] > handValue.handValue.handVal(dealer)[1]:
-                return"You win: " + bet
+                print("You win", bet)
+                return bet
             elif handVal[1] == handValue.handValue.handVal(dealer)[1]:
-                return "Tie"
+                print("Push")
+                return 0
             else: 
-                return "You lose: " + bet
+                print("You lose",bet)
+                return bet * -1 
         # Comparing player's soft count to dealer's hard count
         else:
             if handVal[1] > handValue.handValue.handVal(dealer)[0]:
-                return "You win: " + bet
+                print("You win", bet)
+                return bet
             elif handVal[1] == handValue.handValue.handVal(dealer)[0]:
-                return"Tie"
+                print("Push")
+                return 0
             else: 
-                return"You lose: " + bet
+                print("You lose",bet)
+                return -1 * bet 
             
     # Using player's hard count  
     else: 
         # Comparing player's hard count to dealer's soft count
         if handValue.handValue.handVal(dealer)[0] > 21:
             if handVal[0] > handValue.handValue.handVal(dealer)[1]:
-                return "You win: " + bet
+                print("You win", bet)
+                return bet
             elif handVal[0] == handValue.handValue.handVal(dealer)[1]:
-                return "Tie"
-            else: 
-                return "You lose: " + bet
+                print("Push")
+                return 0
+            else:
+                print("You lose",bet) 
+                return -1 * bet 
         # Comparing player's hard count to dealer's soft count 
         else:
             if handVal[0] > handValue.handValue.handVal(dealer)[0]:
-                return"You win: " + bet
+                print("You win", bet)
+                return bet
             elif handVal[0] == handValue.handValue.handVal(dealer)[0]:
-                return"Tie"
+                print("Push")
+                return 0 
             else: 
-                return"You lose: " +  bet
+                print("You lose",bet) 
+                return -1 * bet 
 
 if __name__ == "__main__":
     main()
