90c37ae3bb69376346357f236c8e67a3c001cff3
Christian Carbeau <ctc53@duke.edu>
2024-01-04 17:24:55 -0500

    started-react

--- App/src/App.js
+++ App/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+//import { hit as Hit} from "/Users/christian/desktop/ML-Blackjack/Back-End/deckL.py"; 
 
 function App() {
+  //const hit = () => {
+    //Hit(); 
+    //console.log()
+  //}
+  const stack = 100;
+  const bet = 10;
+  const dCard1 = "Ten";
+  const dCard2 = "Ace";
+  const pCard1 = "Ten";
+  const pCard2 = "Ace";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "Header"></div>
+        <h1> Welcome to Machine Learning Blackjack</h1>
+      
+      <div className = "Dealer"></div>
+        <h1>Dealer's Hand:</h1>
+        <p>{dCard1} {dCard2}</p>
+      <div className = "player"></div>
+        <p> {pCard1} {pCard2} </p>
+        <p>Bet: {bet} </p>
+        <button> Hit </button>
+        <button> Stay </button>
+        <p>Stack: {stack}</p>
     </div>
   );
 }
